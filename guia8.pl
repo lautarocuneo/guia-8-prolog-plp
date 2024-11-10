b5a86a309b314b964b654d5630f76784afc09282
lautarocuneo <lautarocuneo@gmail.com>
2024-11-10 20:31:48 -0300

    mas cosas
    dxx

--- guia8.pl
+++ guia8.pl
@@ -27,3 +27,17 @@ natural(suc(X)) :- natural(X).
 
 menorOIgual(X, X) :- natural(X).
 menorOIgual(X, suc(Y)) :- menorOIgual(X, Y).
+
+append([], L, L).
+append([X|L1],L2,[X|L3]) :- append(L1,L2,L3).
+
+ultimo(L,U) :- append(_, [U], L).
+
+primero(L,P) :- append([P], _ , L).
+
+ultimo2([U],U).
+ultimo2([_|L],U) :- ultimo2(L,U).
+
+/*da igual el orden en ultimo*/
+ultimo3([_|L],U) :- ultimo3(L,U). 
+ultimo3([U],U).
